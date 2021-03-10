6e9480e28ade00faa5033a4a882eaf4737aa7381
TheBanker1710 <mathieu_lancelle@hotmail.com>
2021-03-10 10:33:55 +0100

    commit serilogging part 1

--- CMS_CORE_NG/Controllers/WeatherForecastController.cs
+++ CMS_CORE_NG/Controllers/WeatherForecastController.cs
@@ -26,14 +26,23 @@ namespace CMS_CORE_NG.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            try
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                var rng = new Random();
+                _logger.LogDebug("Debug message Called Get method WeatherForecastController");
+                _logger.LogInformation("Called Get method WeatherForecastController");
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                }).ToArray();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw ex;
+            }
         }
     }
 }
--- CMS_CORE_NG/Program.cs
+++ CMS_CORE_NG/Program.cs
@@ -1,23 +1,64 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using Serilog;
+using LoggingServices;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CMS_CORE_NG
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    int zero = 0;
+                    int result = 100 / zero;
+                    
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Log.Error("An error occurred while seeding the database  {Error} {StackTrace} {InnerException} {Source}",
+                     ex.Message, ex.StackTrace, ex.InnerException, ex.Source);
+                }
+            }
+
+            host.Run();
+
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+             Host.CreateDefaultBuilder(args)
+                 .ConfigureWebHostDefaults(webBuilder =>
+                 {
+                     webBuilder.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+                       .Enrich.FromLogContext()
+                       .Enrich.WithProperty("Application", "CMS_CORE_NG")
+                       .Enrich.WithProperty("MachineName", Environment.MachineName)
+                       .Enrich.WithProperty("CurrentManagedThreadId", Environment.CurrentManagedThreadId)
+                       .Enrich.WithProperty("OSVersion", Environment.OSVersion)
+                       .Enrich.WithProperty("Version", Environment.Version)
+                       .Enrich.WithProperty("UserName", Environment.UserName)
+                       .Enrich.WithProperty("ProcessId", Process.GetCurrentProcess().Id)
+                       .Enrich.WithProperty("ProcessName", Process.GetCurrentProcess().ProcessName)
+                       .WriteTo.File(formatter: new CustomTextFormatter(), path: Path.Combine(hostingContext.HostingEnvironment.ContentRootPath + $"{Path.DirectorySeparatorChar}Logs{Path.DirectorySeparatorChar}", $"cms_core_ng_{DateTime.Now:yyyyMMdd}.txt"))
+                      .ReadFrom.Configuration(hostingContext.Configuration));
+                     webBuilder.UseStartup<Startup>();
+                 });
     }
 }
